3c8a313c5f53436bdcb398c98f688d4129cd8484
Daniyal Zaheer Khan <154603574+Daniyalzaheerkhan@users.noreply.github.com>
2024-10-21 15:38:17 +0500

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,16 @@
+class FirstClass{
+    public static void main(String args[]){
+        String str1 = "Ali ";
+        String str2 = "is ";
+        String str3 = "saying ";
+        String str4 = "Hello ";
+        String str5 = "World";
+        String add1 = str1.concat(str2);
+        String add2 = add1.concat(str3);
+        String add3 = add2.concat(str4);
+        String add4 = add3.concat(str5);
+        System.out.println("Concatenated String : "+add4);
+        String conv = str4.toUpperCase();
+        System.out.println("4th String in uppercase : "+conv);
+    }
+}
